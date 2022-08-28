ef8ef3fc612c7ae04e4093fe9a5ae9dd83d0414b
gozzy <samuel.jj.gosling@gmail.com>
2022-08-28 18:48:50 +0000

    initialise

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate serde;
+#[macro_use]
+extern crate hex_literal;
+extern crate bellman_ce;
+extern crate rand;
+extern crate itertools;
+extern crate byteorder;
+extern crate num_bigint;
+extern crate num_traits;
+
+pub mod utils;
+pub mod circom_circuit;
+pub mod r1cs_reader;
+pub mod wtns_reader;
